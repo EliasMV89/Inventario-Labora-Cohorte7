90d523ede25e4b1d14f91642894937b805c6f940
EliasMVillagra <eliasmvillagra@gmail.com>
2024-06-14 17:20:16 -0300

    modificacion en DB, main y app.yaml

--- main.go
+++ main.go
@@ -1,3 +1,21 @@
+/*
+package main
+
+import (
+
+	"Inventario/routers"
+	"Inventario/utils"
+	"log"
+	"net/http"
+
+)
+
+	func main() {
+		utils.InitDB()
+		router := routers.InitRouter()
+		log.Fatal(http.ListenAndServe(":8080", router))
+	}
+*/
 package main
 
 import (
@@ -5,10 +23,18 @@ import (
 	"Inventario/utils"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	utils.InitDB()
 	router := routers.InitRouter()
-	log.Fatal(http.ListenAndServe(":8080", router))
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	log.Printf("Starting server on port %s", port)
+	log.Fatal(http.ListenAndServe(":"+port, router))
 }
--- utils/DB.go
+++ utils/DB.go
@@ -1,8 +1,38 @@
+/*
+package utils
+
+import (
+
+	"database/sql"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+
+)
+
+var db *sql.DB
+
+	func InitDB() {
+		var err error
+		db, err = sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/Inventario")
+		if err != nil {
+			log.Fatal(err)
+		}
+		if err = db.Ping(); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	func GetDB() *sql.DB {
+		return db
+	}
+*/
 package utils
 
 import (
 	"database/sql"
 	"log"
+	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -11,7 +41,7 @@ var db *sql.DB
 
 func InitDB() {
 	var err error
-	db, err = sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/Inventario")
+	db, err = sql.Open("mysql", os.Getenv("DB_USER")+":"+os.Getenv("DB_PASSWORD")+"@tcp("+os.Getenv("DB_HOST")+":3306)/"+os.Getenv("DB_NAME"))
 	if err != nil {
 		log.Fatal(err)
 	}
